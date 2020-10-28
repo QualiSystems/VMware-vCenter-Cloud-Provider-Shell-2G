1298992193b2f5afb9eeb7271cac1f3e6f4be323
Anthony Poddubny <anton.p@qualisystems.com>
2020-10-28 23:26:21 +0200

    add old working code from the 1st gen shell

--- src/driver.py
+++ src/driver.py
@@ -1,206 +1,109 @@
-from cloudshell.cp.core.cancellation_manager import CancellationContextManager
-from cloudshell.cp.core.request_actions import DeployedVMActions  # noqa
-from cloudshell.cp.core.request_actions import GetVMDetailsRequestActions  # noqa
-from cloudshell.cp.core.request_actions import DeployVMRequestActions
-from cloudshell.cp.core.reservation_info import ReservationInfo
-from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
-from cloudshell.shell.core.session.logging_session import LoggingSessionContext
+from cloudshell.cp.core import DriverRequestParser
+from cloudshell.cp.core.models import DeployApp, DriverResponse, SaveApp, DeleteSavedApp
+from cloudshell.cp.core.utils import single
 
-from cloudshell.cp.vcenter.api.client import VCenterAPIClient
-from cloudshell.cp.vcenter.flows.autoload import VCenterAutoloadFlow
-from cloudshell.cp.vcenter.flows.deploy_vm.vm_from_vm import VCenterDeployVMfromVMFlow
-from cloudshell.cp.vcenter.models import deploy_app
-from cloudshell.cp.vcenter.resource_config import VCenterResourceConfig
+from cloudshell.cp.vcenter.commands.command_orchestrator import CommandOrchestrator
+# from cloudshell.shell.core.context import ResourceCommandContext, CancellationContext
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.cp.vcenter.common.vcenter.model_auto_discovery import VCenterAutoModelDiscovery
 
 
 class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
+    def cleanup(self):
+        pass
+
     def __init__(self):
         """Init function.
 
         ctor must be without arguments, it is created with reflection at run time
         """
+        self.request_parser = DriverRequestParser()
+        self.command_orchestrator = CommandOrchestrator()  # type: CommandOrchestrator
+        self.deployments = dict()
+        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From VM 2G'] = self.deploy_clone_from_vm
+        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From Linked Clone 2G'] = self.deploy_from_linked_clone
+        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From Template 2G'] = self.deploy_from_template
+        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From Image 2G'] = self.deploy_from_image
+
+    def initialize(self, **kwargs):
         pass
-
-    def initialize(self, context):
-        """Called every time a new instance of the driver is created.
-
-        This method can be left unimplemented but this is a good place to
-        load and cache the driver configuration, initiate sessions etc.
-        Whatever you choose, do not remove it.
-        :param InitCommandContext context: the context the command runs on
-        """
-        pass
-
-    def get_inventory(self, context):
-        """Called when the cloud provider resource is created in the inventory.
-
-        Method validates the values of the cloud provider attributes, entered by
-        the user as part of the cloud provider resource creation. In addition,
-        this would be the place to assign values programmatically to optional
-        attributes that were not given a value by the user. If one of the
-        validations failed, the method should raise an exception
-        :param AutoLoadCommandContext context: the context the command runs on
-        :return Attribute and sub-resource information for the Shell resource
-        you can return an AutoLoadDetails object
-        :rtype: AutoLoadDetails
-        """
-        with LoggingSessionContext(context) as logger:
-            logger.info("Starting Autoload command...")
-            api = CloudShellSessionContext(context).get_api()
-            resource_config = VCenterResourceConfig.from_context(
-                context=context, api=api
-            )
-
-            vcenter_client = VCenterAPIClient(
-                host=resource_config.address,
-                user=resource_config.user,
-                password=resource_config.password,
-                logger=logger,
-            )
-
-            autoload_flow = VCenterAutoloadFlow(
-                resource_config=resource_config,
-                vcenter_client=vcenter_client,
-                logger=logger,
-            )
-
-            return autoload_flow.discover()
-
-    def Deploy(self, context, request, cancellation_context=None):
-        """Called when reserving a sandbox during setup.
-
-        Method creates the compute resource in the cloud provider -
-        VM instance or container. If App deployment fails, return a
-        "success false" action result.
-        :param ResourceCommandContext context:
-        :param str request: A JSON string with the list of requested
-        deployment actions
-        :param CancellationContext cancellation_context:
-        :return:
-        :rtype: str
-        """
-        with LoggingSessionContext(context) as logger:
-            logger.info("Starting Deploy command...")
-            logger.debug(f"Request: {request}")
-            api = CloudShellSessionContext(context).get_api()
-            resource_config = VCenterResourceConfig.from_context(
-                context=context, api=api
-            )
-
-            cancellation_manager = CancellationContextManager(cancellation_context)
-            reservation_info = ReservationInfo.from_resource_context(context)
-            vcenter_client = VCenterAPIClient(
-                host=resource_config.address,
-                user=resource_config.user,
-                password=resource_config.password,
-                logger=logger,
-            )
-
-            for deploy_app_cls in (
-                deploy_app.VMFromVMDeployApp,
-                deploy_app.VMFromTemplateDeployApp,
-                deploy_app.VMFromLinkedCloneDeployApp,
-                deploy_app.VMFromImageDeployApp,
-            ):
-
-                DeployVMRequestActions.register_deployment_path(deploy_app_cls)
-
-            request_actions = DeployVMRequestActions.from_request(
-                request=request, cs_api=api
-            )
-
-            deploy_flow = VCenterDeployVMfromVMFlow(
-                resource_config=resource_config,
-                reservation_info=reservation_info,
-                vcenter_client=vcenter_client,
-                cs_api=api,
-                cancellation_manager=cancellation_manager,
-                logger=logger,
-            )
-
-            return deploy_flow.deploy(request_actions=request_actions)
-
-    def PowerOn(self, context, ports):
-        """Called when reserving a sandbox during setup.
-
-        Call for each app in the sandbox can also be run manually by
-        the sandbox end-user from the deployed App's commands pane.
-        Method spins up the VM If the operation fails, method should raise an exception.
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        """
-        with LoggingSessionContext(context) as logger:
-            logger.info("Starting Power On command...")
-            api = CloudShellSessionContext(context).get_api()  # noqa
-
-    def PowerOff(self, context, ports):
-        """Called during sandbox's teardown.
-
-        Can also be run manually by the sandbox end-user from the deployed
-        App's commands pane. Method shuts down (or powers off) the VM instance.
-        If the operation fails, method should raise an exception.
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        """
-        with LoggingSessionContext(context) as logger:
-            logger.info("Starting Power Off command...")
-            api = CloudShellSessionContext(context).get_api()  # noqa
-
-    def PowerCycle(self, context, ports, delay):
-        pass
-
-    def remote_refresh_ip(self, context, ports, cancellation_context):
-        """Called when reserving a sandbox during setup.
-
-        Call for each app in the sandbox can also be run manually by the
-        sandbox end-user from the deployed App's commands pane.
-        Method retrieves the VM's updated IP address from the cloud provider
-        and sets it on the deployed App resource. Both private and public IPs
-        are retrieved, as appropriate. If the operation fails, method should
-        raise an exception.
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        :param CancellationContext cancellation_context:
-        :return:
-        """
-        with LoggingSessionContext(context) as logger:
-            logger.info("Starting Remote Refresh IP command...")
-            api = CloudShellSessionContext(context).get_api()  # noqa
-
-    def GetVmDetails(self, context, requests, cancellation_context):
-        """Called when reserving a sandbox during setup.
-
-        Call for each app in the sandbox can also be run manually by the sandbox
-        end-user from the deployed App's VM Details pane. Method queries
-        cloud provider for instance operating system, specifications and networking
-        information and returns that as a json serialized driver response
-        containing a list of VmDetailsData. If the operation fails,
-        method should raise an exception.
-        :param ResourceCommandContext context:
-        :param str requests:
-        :param CancellationContext cancellation_context:
-        :return:
-        """
-        with LoggingSessionContext(context) as logger:
-            logger.info("Starting Get VM Details command...")
-            logger.debug(f"Requests: {requests}")
-            api = CloudShellSessionContext(context).get_api()  # noqa
 
     def ApplyConnectivityChanges(self, context, request):
-        pass
+        return self.command_orchestrator.connect_bulk(context, request)
 
     def disconnect_all(self, context, ports):
-        pass
+        return self.command_orchestrator.disconnect_all(context, ports)
 
     def disconnect(self, context, ports, network_name):
-        pass
+        return self.command_orchestrator.disconnect(context, ports, network_name)
+
+    def DeleteInstance(self, context, ports):
+        return self.command_orchestrator.DeleteInstance(context, ports)
+
+    def remote_refresh_ip(self, context, cancellation_context, ports):
+        return self.command_orchestrator.refresh_ip(context, cancellation_context, ports)
+
+    def PowerOff(self, context, ports):
+        return self.command_orchestrator.power_off(context, ports)
+
+    # the name is by the Qualisystems conventions
+    def PowerOn(self, context, ports):
+        """
+        Powers off the remote vm
+        :param models.QualiDriverModels.ResourceRemoteCommandContext context: the context the command runs on
+        :param list[string] ports: the ports of the connection between the remote resource and the local resource, NOT IN USE!!!
+        """
+        return self.command_orchestrator.power_on(context, ports)
+
+    # the name is by the Qualisystems conventions
+    def PowerCycle(self, context, ports, delay):
+        return self.command_orchestrator.power_cycle(context, ports, delay)
+
+    def Deploy(self, context, request=None, cancellation_context=None):
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        deploy_action = single(actions, lambda x: isinstance(x, DeployApp))
+        deployment_name = deploy_action.actionParams.deployment.deploymentPath
+
+        if deployment_name in self.deployments.keys():
+            deploy_method = self.deployments[deployment_name]
+            deploy_result = deploy_method(context, deploy_action, cancellation_context)
+            return DriverResponse([deploy_result]).to_driver_response_json()
+        else:
+            raise Exception('Could not find the deployment')
 
     def SaveApp(self, context, request, cancellation_context=None):
-        pass
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        save_actions = [x for x in actions if isinstance(x, SaveApp)]
+        save_app_results = self.command_orchestrator.save_sandbox(context, save_actions, cancellation_context)
+        return DriverResponse(save_app_results).to_driver_response_json()
 
     def DeleteSavedApps(self, context, request, cancellation_context=None):
-        pass
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        delete_actions = [x for x in actions if isinstance(x, DeleteSavedApp)]
+        save_app_results = self.command_orchestrator.delete_saved_sandbox(context, delete_actions, cancellation_context)
+        return DriverResponse(save_app_results).to_driver_response_json()
+
+    def deploy_from_template(self, context, deploy_action, cancellation_context):
+        return self.command_orchestrator.deploy_from_template(context, deploy_action, cancellation_context)
+
+    def deploy_clone_from_vm(self, context, deploy_action, cancellation_context):
+        return self.command_orchestrator.deploy_clone_from_vm(context, deploy_action, cancellation_context)
+
+    def deploy_from_linked_clone(self, context, deploy_action, cancellation_context):
+        return self.command_orchestrator.deploy_from_linked_clone(context, deploy_action, cancellation_context)
+
+    def deploy_from_image(self, context, deploy_action, cancellation_context):
+        if cancellation_context is None:
+            cancellation_context = CancellationContext()
+        return self.command_orchestrator.deploy_from_image(context, deploy_action, cancellation_context)
+
+    def get_inventory(self, context):
+        """
+        :type context: models.QualiDriverModels.AutoLoadCommandContext
+        """
+
+        validator = VCenterAutoModelDiscovery()
+        return validator.validate_and_discover(context)
 
     def remote_save_snapshot(self, context, ports, snapshot_name, save_memory):
         """Saves virtual machine to a snapshot.
@@ -216,11 +119,11 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         :type save_memory: str
         :return:
         """
-        pass
+        return self.command_orchestrator.save_snapshot(context, snapshot_name, save_memory)
 
     def remote_restore_snapshot(self, context, ports, snapshot_name):
-        """Restores virtual machine from a snapshot.
-
+        """
+        Restores virtual machine from a snapshot
         :param context: resource context of the vCenterShell
         :type context: models.QualiDriverModels.ResourceCommandContext
         :param ports:list[string] ports: the ports of the connection between
@@ -230,11 +133,11 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         :type snapshot_name: str
         :return:
         """
-        pass
+        return self.command_orchestrator.restore_snapshot(context, snapshot_name)
 
     def remote_get_snapshots(self, context, ports):
-        """Returns list of snapshots.
-
+        """
+        Returns list of snapshots
         :param context: resource context of the vCenterShell
         :type context: models.QualiDriverModels.ResourceCommandContext
         :param ports:list[string] ports: the ports of the connection between
@@ -242,34 +145,17 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         :type ports: list[string]
         :return: list
         """
-        pass
+        return self.command_orchestrator.get_snapshots(context)
 
     def orchestration_save(self, context, ports, mode="shallow", custom_params=None):
-        pass
+        return self.command_orchestrator.orchestration_save(context, mode, custom_params)
 
     def orchestration_restore(self, context, ports, saved_details):
-        pass
+        return self.command_orchestrator.orchestration_restore(context, saved_details)
 
     def get_vm_uuid(self, context, vm_name):
-        pass
+        return self.command_orchestrator.get_vm_uuid_by_name(context, vm_name)
 
-    def DeleteInstance(self, context, ports):
-        """Called when removing a deployed App from the sandbox.
+    def GetVmDetails(self, context, cancellation_context, requests):
+        return self.command_orchestrator.get_vm_details(context, cancellation_context, requests)
 
-        Method deletes the VM from the cloud provider. If the operation fails,
-        method should raise an exception.
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        """
-        with LoggingSessionContext(context) as logger:
-            logger.info("Starting Delete Instance command...")
-            api = CloudShellSessionContext(context).get_api()  # noqa
-
-    def cleanup(self):
-        """Destroy the driver session.
-
-        This function is called every time a driver instance is destroyed.
-        This is a good place to close any open sessions, finish writing
-        to log files, etc.
-        """
-        pass
