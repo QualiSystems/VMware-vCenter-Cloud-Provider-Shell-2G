828ca0e22a696301409858291dac45e7924d7190
Anthony Poddubny <anton.p@qualisystems.com>
2020-11-12 16:05:20 +0200

    fix tox pre-commit

--- src/driver.py
+++ src/driver.py
@@ -1,11 +1,12 @@
 from cloudshell.cp.core import DriverRequestParser
-from cloudshell.cp.core.models import DeployApp, DriverResponse, SaveApp, DeleteSavedApp
+from cloudshell.cp.core.models import DeleteSavedApp, DeployApp, DriverResponse, SaveApp
 from cloudshell.cp.core.utils import single
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
 
 from cloudshell.cp.vcenter.commands.command_orchestrator import CommandOrchestrator
-# from cloudshell.shell.core.context import ResourceCommandContext, CancellationContext
-from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.cp.vcenter.common.vcenter.model_auto_discovery import VCenterAutoModelDiscovery
+from cloudshell.cp.vcenter.common.vcenter.model_auto_discovery import (
+    VCenterAutoModelDiscovery,
+)
 
 
 class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
@@ -19,11 +20,19 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         """
         self.request_parser = DriverRequestParser()
         self.command_orchestrator = CommandOrchestrator()  # type: CommandOrchestrator
-        self.deployments = dict()
-        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From VM 2G'] = self.deploy_clone_from_vm
-        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From Linked Clone 2G'] = self.deploy_from_linked_clone
-        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From Template 2G'] = self.deploy_from_template
-        self.deployments['VMware vCenter Cloud Provider 2G.vCenter VM From Image 2G'] = self.deploy_from_image
+        self.deployments = {}
+        self.deployments[
+            "VMware vCenter Cloud Provider 2G.vCenter VM From VM 2G"
+        ] = self.deploy_clone_from_vm
+        self.deployments[
+            "VMware vCenter Cloud Provider 2G.vCenter VM From Linked Clone 2G"
+        ] = self.deploy_from_linked_clone
+        self.deployments[
+            "VMware vCenter Cloud Provider 2G.vCenter VM From Template 2G"
+        ] = self.deploy_from_template
+        self.deployments[
+            "VMware vCenter Cloud Provider 2G.vCenter VM From Image 2G"
+        ] = self.deploy_from_image
 
     def initialize(self, **kwargs):
         pass
@@ -41,17 +50,21 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         return self.command_orchestrator.DeleteInstance(context, ports)
 
     def remote_refresh_ip(self, context, cancellation_context, ports):
-        return self.command_orchestrator.refresh_ip(context, cancellation_context, ports)
+        return self.command_orchestrator.refresh_ip(
+            context, cancellation_context, ports
+        )
 
     def PowerOff(self, context, ports):
         return self.command_orchestrator.power_off(context, ports)
 
     # the name is by the Qualisystems conventions
     def PowerOn(self, context, ports):
-        """
-        Powers off the remote vm
-        :param models.QualiDriverModels.ResourceRemoteCommandContext context: the context the command runs on
-        :param list[string] ports: the ports of the connection between the remote resource and the local resource, NOT IN USE!!!
+        """Powers off the remote vm.
+
+        :param models.QualiDriverModels.ResourceRemoteCommandContext context:
+            the context the command runs on
+        :param list[string] ports: the ports of the connection between the remote
+            resource and the local resource, NOT IN USE!!!
         """
         return self.command_orchestrator.power_on(context, ports)
 
@@ -69,39 +82,51 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
             deploy_result = deploy_method(context, deploy_action, cancellation_context)
             return DriverResponse([deploy_result]).to_driver_response_json()
         else:
-            raise Exception('Could not find the deployment')
+            raise Exception("Could not find the deployment")
 
     def SaveApp(self, context, request, cancellation_context=None):
         actions = self.request_parser.convert_driver_request_to_actions(request)
         save_actions = [x for x in actions if isinstance(x, SaveApp)]
-        save_app_results = self.command_orchestrator.save_sandbox(context, save_actions, cancellation_context)
+        save_app_results = self.command_orchestrator.save_sandbox(
+            context, save_actions, cancellation_context
+        )
         return DriverResponse(save_app_results).to_driver_response_json()
 
     def DeleteSavedApps(self, context, request, cancellation_context=None):
         actions = self.request_parser.convert_driver_request_to_actions(request)
         delete_actions = [x for x in actions if isinstance(x, DeleteSavedApp)]
-        save_app_results = self.command_orchestrator.delete_saved_sandbox(context, delete_actions, cancellation_context)
+        save_app_results = self.command_orchestrator.delete_saved_sandbox(
+            context, delete_actions, cancellation_context
+        )
         return DriverResponse(save_app_results).to_driver_response_json()
 
     def deploy_from_template(self, context, deploy_action, cancellation_context):
-        return self.command_orchestrator.deploy_from_template(context, deploy_action, cancellation_context)
+        return self.command_orchestrator.deploy_from_template(
+            context, deploy_action, cancellation_context
+        )
 
     def deploy_clone_from_vm(self, context, deploy_action, cancellation_context):
-        return self.command_orchestrator.deploy_clone_from_vm(context, deploy_action, cancellation_context)
+        return self.command_orchestrator.deploy_clone_from_vm(
+            context, deploy_action, cancellation_context
+        )
 
     def deploy_from_linked_clone(self, context, deploy_action, cancellation_context):
-        return self.command_orchestrator.deploy_from_linked_clone(context, deploy_action, cancellation_context)
+        return self.command_orchestrator.deploy_from_linked_clone(
+            context, deploy_action, cancellation_context
+        )
 
     def deploy_from_image(self, context, deploy_action, cancellation_context):
         if cancellation_context is None:
-            cancellation_context = CancellationContext()
-        return self.command_orchestrator.deploy_from_image(context, deploy_action, cancellation_context)
+            cancellation_context = CancellationContext()  # noqa: F821
+        return self.command_orchestrator.deploy_from_image(
+            context, deploy_action, cancellation_context
+        )
 
     def get_inventory(self, context):
-        """
+        """Get inventory.
+
         :type context: models.QualiDriverModels.AutoLoadCommandContext
         """
-
         validator = VCenterAutoModelDiscovery()
         return validator.validate_and_discover(context)
 
@@ -111,7 +136,7 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         :param context: resource context of the vCenterShell
         :type context: models.QualiDriverModels.ResourceCommandContext
         :param ports:list[string] ports: the ports of the connection between the
-         remote resource and the local resource
+        remote resource and the local resource
         :type ports: list[string]
         :param snapshot_name: snapshot name to save to
         :type snapshot_name: str
@@ -119,11 +144,13 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         :type save_memory: str
         :return:
         """
-        return self.command_orchestrator.save_snapshot(context, snapshot_name, save_memory)
+        return self.command_orchestrator.save_snapshot(
+            context, snapshot_name, save_memory
+        )
 
     def remote_restore_snapshot(self, context, ports, snapshot_name):
-        """
-        Restores virtual machine from a snapshot
+        """Restores virtual machine from a snapshot.
+
         :param context: resource context of the vCenterShell
         :type context: models.QualiDriverModels.ResourceCommandContext
         :param ports:list[string] ports: the ports of the connection between
@@ -136,8 +163,8 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         return self.command_orchestrator.restore_snapshot(context, snapshot_name)
 
     def remote_get_snapshots(self, context, ports):
-        """
-        Returns list of snapshots
+        """Returns list of snapshots.
+
         :param context: resource context of the vCenterShell
         :type context: models.QualiDriverModels.ResourceCommandContext
         :param ports:list[string] ports: the ports of the connection between
@@ -148,7 +175,9 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         return self.command_orchestrator.get_snapshots(context)
 
     def orchestration_save(self, context, ports, mode="shallow", custom_params=None):
-        return self.command_orchestrator.orchestration_save(context, mode, custom_params)
+        return self.command_orchestrator.orchestration_save(
+            context, mode, custom_params
+        )
 
     def orchestration_restore(self, context, ports, saved_details):
         return self.command_orchestrator.orchestration_restore(context, saved_details)
@@ -157,5 +186,6 @@ class VMwarevCenterCloudProviderShell2GDriver(ResourceDriverInterface):
         return self.command_orchestrator.get_vm_uuid_by_name(context, vm_name)
 
     def GetVmDetails(self, context, cancellation_context, requests):
-        return self.command_orchestrator.get_vm_details(context, cancellation_context, requests)
-
+        return self.command_orchestrator.get_vm_details(
+            context, cancellation_context, requests
+        )
